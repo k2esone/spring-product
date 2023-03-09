ea9b05d97758c39f444ea72df2956a109727a2a6
Dominik Kowalczyk <dkowalczyk.lwc@gmail.com>
2023-03-09 21:19:53 +0100

    first commit

---
+++ src/main/java/pl/sda/springproduct/SpringProductApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.springproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProductApplication.class, args);
+    }
+
+}
